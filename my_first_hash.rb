ad7b23ff13672274802dd98c2eaa7974e472a60a
Emilio Betancourt <embeor@gmail.com>
2018-10-11 01:17:23 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,13 +8,13 @@ def shipping_manifest
 the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
-def retrieval (find)
+def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-   shipping_manifest = ["find"]
+   shipping_manifest = ["oil paintings"]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
